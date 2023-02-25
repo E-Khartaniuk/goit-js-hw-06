fe0fcbaa9b1e4ed1f883b4061c70c7aea01312be
Evhenii Khartaniuk <co6alt@gmail.com>
2023-02-26 00:26:48 +0200

    9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,16 @@
+const bgColor = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", getRandomHexColor);
+button.addEventListener("click", randomBGColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function randomBGColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  bgColor.textContent = getRandomHexColor();
 }
